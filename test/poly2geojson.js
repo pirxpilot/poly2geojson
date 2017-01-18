5716136a007c075ec89c65933c3fdf4316d262ff
Damian Krzeminski <pirxpilot@code42day.com>
2017-01-17 19:47:59 -0800

    use GeoJSON MultiPolygon Fixture as an output format

--- test/poly2geojson.js
+++ test/poly2geojson.js
@@ -3,17 +3,44 @@ var split = require('split');
 var poly2geojson = require('../');
 
 describe('poly2geojson', function () {
-  it('should parrse poly file', function (done) {
+  it('should parse poly file with single ring', function (done) {
     var ref = require('./fixtures/china.json');
 
     fs.createReadStream(__dirname + '/fixtures/china.poly')
       .pipe(split())
       .pipe(poly2geojson())
       .on('data', function(geojson) {
-        geojson.should.have.property('type', 'Polygon');
-        geojson.should.have.property('coordinates').with.length(1);
+        geojson.should.have.property('type', 'Feature');
+        geojson.should.have.property('properties', { name: 'china' });
+        geojson.should.have.property('geometry');
+
+        var geometry = geojson.geometry;
+        geometry.should.have.property('type', 'MultiPolygon');
+        geometry.should.have.property('coordinates').with.length(1);
+
         geojson.should.eql(ref);
         done();
       });
   });
+
+  it('should parse poly file with multiple rings', function (done) {
+    var ref = require('./fixtures/australia.json');
+
+    fs.createReadStream(__dirname + '/fixtures/australia.poly')
+      .pipe(split())
+      .pipe(poly2geojson())
+      .on('data', function(geojson) {
+        geojson.should.have.property('type', 'Feature');
+        geojson.should.have.property('properties', { name: 'australia' });
+        geojson.should.have.property('geometry');
+
+        var geometry = geojson.geometry;
+        geometry.should.have.property('type', 'MultiPolygon');
+        geometry.should.have.property('coordinates').with.length(1);
+
+        geojson.should.eql(ref);
+        done();
+      });
+  });
+
 });
